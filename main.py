ad6603f05236f74c005b03bc428cfc98cfc39b95
Habakkuk Onchiri <73702963+archhub868@users.noreply.github.com>
2024-03-13 07:17:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Create a tuple of book names
+books = (
+    "Pride and Prejudice",
+    "To Kill a Mockingbird",
+    "The Hitchhiker's Guide to the Galaxy",
+    "Frankenstein",
+    "The Lord of the Rings"
+)
+
+# Print each book name on a new line
+for book in books:
+    print(book)
